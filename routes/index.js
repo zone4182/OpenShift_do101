c4ec1dbb706f74fbef26d072e0619db025606107
Bart van der Hoek <bart.van.der.hoek@crv4all.com>
2020-06-09 09:16:51 +0200

    Just some message changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Express Project for Openshift lessons' });
 });
 
 module.exports = router;
